f34d63c587eb0e75de117ca5f27d2523a5fcf6f3
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-09-14 15:55:07 -0300

    removendo registros

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -64,6 +64,9 @@ require 'tarefa_controller.php'; //recuperando tarefa_controller do repositório
 			tarefa.insertBefore(form, tarefa[0])//árvore de elementos que será adicionada, nó dentro de tarefa que a árvore de elementos será adicionada
 			//insertBefore --- insert após a renderização da página (método nativo da API do DOM)
 		}
+		function remover(id) {
+			location.href = 'todas_tarefas.php?acao=remover&id=' + id;//redirecionando para página com todas as tarefas + concatenando com o id passado por parâmetro
+		}
 	</script>
 </head>
 
@@ -102,7 +105,8 @@ require 'tarefa_controller.php'; //recuperando tarefa_controller do repositório
 										<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
 									</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
+										<i class="fas fa-trash-alt fa-lg text-danger"
+											onclick="remover(<?= $tarefa->id ?>)"></i>
 										<i class="fas fa-edit fa-lg text-info"
 											onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
